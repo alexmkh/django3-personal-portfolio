bb7c61ff54cca5aa24f18c46d08b0e4ab00563e0
alexmkh <alexmkh@ukr.net>
2022-12-05 12:30:59 +0200

    Lesson 47 completed.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -8,3 +8,11 @@ def all_blogs(request):
         'blogs': blogs
     }
     return render(request, 'blog/blog.html', context)
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    # blog = Blog.objects.get(id=blog_id)
+    context = {
+        'blog': blog
+    }
+    return render(request, 'blog/detail.html', context)
